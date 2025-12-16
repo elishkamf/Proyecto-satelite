28e341028e0d3c816bf9bf4f8c0b919cc03906b1
elishkamf <elishkamf@gmail.com>
2025-12-16 08:33:31 +0100

    Refactor receptor_com3.ino for IR commands handling

--- receptor_com3.ino
+++ receptor_com3.ino
@@ -1,11 +1,23 @@
 #include <SoftwareSerial.h>
+#include <IRremote.h>
 
 
-#define RX_PIN 10  
-#define TX_PIN 11  
-#define LED_VERDE 13    
-#define LED_AMARILLO 7  
-#define LED_ROJO 6      
+// ---------- PINES ----------
+#define RX_PIN 10     // RX desde EMISOR
+#define TX_PIN 11     // TX hacia EMISOR
+#define LED_VERDE 13
+#define LED_AMARILLO 7
+#define LED_ROJO 6
+#define IR_PIN 5
+
+
+#define IR_CODE_TEMP   0xF30CFF00  // Modo TEMP/HUM  Boton 1
+#define IR_CODE_RADAR  0xE718FF00  // RADAR  Boton 2
+#define IR_CODE_ORBITA 0xA15EFF00 // ORBITAL  Boton 3
+#define IR_CODE_PAUSA  0xF708FF00 // PAUSA   Boton 4
+#define IR_CODE_STOP   0xE31CFF00  // STOP Boton 5
+
+
 
 
 SoftwareSerial emisor(RX_PIN, TX_PIN);
@@ -27,8 +39,9 @@ uint8_t calcularChecksum(String mensaje) {
 
 
 void setup() {
-  Serial.begin(9600);
-  emisor.begin(9600);
+  Serial.begin(9600);      // USB hacia PC/Python
+  emisor.begin(9600);      // Enlace serie hacia EMISOR
+  IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
 
 
   pinMode(LED_VERDE, OUTPUT);
@@ -39,7 +52,7 @@ void setup() {
   Serial.println("🌍 Estación Tierra lista");
 
 
-  // Enviar comando inicial (opcional)
+  // Enviar comando inicial (opcional) al EMISOR
   emisor.print('T');
   digitalWrite(LED_VERDE, HIGH);
   delay(500);
@@ -48,7 +61,9 @@ void setup() {
 
 
 void loop() {
-  // --- Reenviar comandos del PC al emisor por LoRa ---
+
+
+  // --- 1) Reenviar comandos del PC al emisor ---
   if (Serial.available()) {
     char comando = Serial.read();
     if (comando == 'T' || comando == 'D' || comando == 'U' || comando == 'P' || comando == 'O') {
@@ -57,7 +72,60 @@ void loop() {
   }
 
 
-  // --- Procesar mensajes del emisor ---
+  // --- 2) COMANDOS DESDE MANDO IR ---
+  if (IrReceiver.decode()) {
+    unsigned long code = IrReceiver.decodedIRData.decodedRawData;
+
+
+    // Según el botón IR, enviar comando al EMISOR
+    // y además mandar etiqueta IR_... a Python
+    if (code == IR_CODE_TEMP) {
+      emisor.print('T');
+      String msg = "IR_TEMP";
+      uint8_t cs = calcularChecksum(msg);
+      Serial.print(msg);
+      Serial.print("*");
+      Serial.println(cs);
+    }
+    else if (code == IR_CODE_RADAR) {
+      emisor.print('U');
+      String msg = "IR_RADAR";
+      uint8_t cs = calcularChecksum(msg);
+      Serial.print(msg);
+      Serial.print("*");
+      Serial.println(cs);
+    }
+    else if (code == IR_CODE_ORBITA) {
+      emisor.print('O');
+      String msg = "IR_ORBITAL";
+      uint8_t cs = calcularChecksum(msg);
+      Serial.print(msg);
+      Serial.print("*");
+      Serial.println(cs);
+    }
+    else if (code == IR_CODE_PAUSA) {
+      emisor.print('P');
+      String msg = "IR_PAUSA";
+      uint8_t cs = calcularChecksum(msg);
+      Serial.print(msg);
+      Serial.print("*");
+      Serial.println(cs);
+    }
+    else if (code == IR_CODE_STOP) {
+      emisor.print('T');
+      String msg = "IR_STOP";
+      uint8_t cs = calcularChecksum(msg);
+      Serial.print(msg);
+      Serial.print("*");
+      Serial.println(cs);
+    }
+
+
+    IrReceiver.resume();
+  }
+
+
+  // --- 3) Procesar mensajes del EMISOR ---
   if (emisor.available()) {
     String linea = emisor.readStringUntil('\n');
     linea.trim();
@@ -87,14 +155,15 @@ void loop() {
 
     // Enviar datos válidos al PC (Python)
     Serial.println(datos);
-   
+
+
     // Resetear temporizador de fallo
     ultimoMensaje = millis();
     falloComunicacion = false;
     digitalWrite(LED_AMARILLO, LOW);
 
 
-    // LED verde para datos de temperatura/humedad válidos
+    // LED verde para datos de temperatura/humedad válidos (formato h,t)
     int comaIndex = datos.indexOf(',');
     if (comaIndex > 0) {
       String tempStr = datos.substring(comaIndex + 1);
@@ -122,9 +191,12 @@ void loop() {
   }
 
 
-  // --- Detección de timeout ---
+  // --- 4) Detección de timeout ---
   if (millis() - ultimoMensaje > 5000 && !falloComunicacion) {
     falloComunicacion = true;
     digitalWrite(LED_AMARILLO, HIGH);
   }
 }
+
+
+
